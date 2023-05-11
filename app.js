32593b7e526e643c6493ceb8c6bfc09299b5be97
Lennie Nurse <lnursej@gmail.com>
2023-05-11 15:21:33 -0400

    updated app.js

--- app.js
+++ app.js
@@ -2,20 +2,12 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
-<<<<<<< HEAD
-const listingsController = require("./controllers/listingsController")
-// const discussionController = require("./controllers/discussion_boardsController")
-const categoriesController = require("./controllers/categoriesController")
-const commentsController = require("./controllers/commentsController")
-const usersController = require("./controllers/usersController")
-=======
 const usersController = require("./controllers/usersController")
 const listingsController = require("./controllers/listingsController")
 const discussionController = require("./controllers/discussion_boardsController")
-// const categoriesController = require("./controllers/categoriesController")
-// const commentsController = require("./controllers/commentsController")
+const categoriesController = require("./controllers/categoriesController")
+const commentsController = require("./controllers/commentsController")
 const jobsController = require("./controllers/jobsController")
->>>>>>> a4e60dd31698dc9c4e4d0e2016c559b8296197db
 
 //MIDDLEWARE
 app.use(express.json())
@@ -23,17 +15,11 @@ app.use(cors())
 
 //routes
 app.use("/users", usersController)
-<<<<<<< HEAD
 app.use("/categories", categoriesController)
 app.use("/comments", commentsController)
-// app.use("/discussion_boards", discussionController)
-=======
-// app.use("/categories", categoriesController)
-// app.use("/comments", commentsController)
 app.use("/discussions", discussionController)
->>>>>>> a4e60dd31698dc9c4e4d0e2016c559b8296197db
 app.use("/listings", listingsController)
-app.use('/jobs', jobsController)
+app.use("/jobs", jobsController)
 
 app.get("/", (req, res) => {
   res.send("Welcome to Out&Abt!!")
